seabat/serverless-bss

Navigation Drawer Activity プロジェクトを新規作成

app/src/main/java/dev/seabat/android/serverlessbbs/MainActivity.kt
@@ -1,0 +1,73 @@
+package dev.seabat.android.serverlessbbs
+
+import android.os.Bundle
+import android.view.Menu
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.navigation.NavigationView
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import dev.seabat.android.serverlessbbs.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        this.setupFab(binding)
+        appBarConfiguration = this.setupDrawer(binding)
+        this.setupToolbar(binding, appBarConfiguration)
+        this.setupNavigation(binding)
+    }
+
+    private fun setupFab(binding: ActivityMainBinding) {
+        binding.appBarMain.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+    private fun setupDrawer(binding: ActivityMainBinding): AppBarConfiguration {
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        return AppBarConfiguration(
+            setOf(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+            ), drawerLayout
+        )
+    }
+
+    private fun setupToolbar(binding: ActivityMainBinding, appBarConfiguration: AppBarConfiguration) {
+        setSupportActionBar(binding.appBarMain.toolbar)
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        findViewById<Toolbar>(R.id.toolbar)
+            .setupWithNavController(navController, appBarConfiguration)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    private fun setupNavigation(binding: ActivityMainBinding) {
+        val navView: NavigationView = binding.navView
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        navView.setupWithNavController(navController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+}

seabat/serverless-bbs

app/src/main/java/dev/seabat/android/serverlessbbs/MainActivity.kt
@@ -1,0 +1,71 @@
+package dev.seabat.android.serverlessbbs
+
+import android.os.Bundle
+import android.view.Menu
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.navigation.NavigationView
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.appcompat.app.AppCompatActivity
+import dev.seabat.android.serverlessbbs.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        this.setupFab(binding)
+        appBarConfiguration = this.createAppBarConfigWithDrawer(binding)
+        this.setupToolbar(binding, appBarConfiguration)
+        this.setupNavigation(binding)
+    }
+
+    private fun setupFab(binding: ActivityMainBinding) {
+        binding.appBarMain.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+    private fun createAppBarConfigWithDrawer(binding: ActivityMainBinding): AppBarConfiguration {
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        return AppBarConfiguration(
+            setOf(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+            ), drawerLayout
+        )
+    }
+
+    private fun setupToolbar(binding: ActivityMainBinding, appBarConfiguration: AppBarConfiguration) {
+        setSupportActionBar(binding.appBarMain.toolbar)
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        binding.appBarMain.toolbar.setupWithNavController(navController, appBarConfiguration)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    private fun setupNavigation(binding: ActivityMainBinding) {
+        val navView: NavigationView = binding.navView
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        navView.setupWithNavController(navController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+}